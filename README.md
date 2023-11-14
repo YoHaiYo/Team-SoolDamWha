a7119f1f55b8d888214e31c2cbee580d58fafaf4
User -2023UFLUK\Administrator <ss@ssmail.com>
2023-11-14 15:47:30 +0900

    1차 merge 작업

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Team-SolDamWha
+# 프로젝트 소개
+**Team-SolDamWha**
 프론트엔드반 술담화 프로젝트 방입니다.
 
-**팀원**
+# 배포용링크(임시)
+https://yohaiyo.github.io/Team-SoolDamWha/
+
+# 팀원 소개
 
 jiyy25
 https://github.com/jiyy25
@@ -15,5 +19,3 @@ https://github.com/SeungEunChun
 YoHaiYo
 https://github.com/YoHaiYo
 
-**배포용링크(임시)**
-https://yohaiyo.github.io/Team-SoolDamWha/
--- js/main.js
+++ js/main.js
@@ -1,19 +1,79 @@
-const promotionSwiper = new Swiper('#promotionSwiper.swiper', {
-  // 자동롤링 4초마다
+var swiperMain = new Swiper('#promotionSwiper .swiper', {
+
+
+  loop: true,
+
+
+  effect: "fade",
+  speed: 2000,
+
+  centeredSlides: true,
+
+
+
+  navigation: {
+    nextEl: '#promotionSwiper .swiper-button-next',
+    prevEl: '#promotionSwiper .swiper-button-prev',
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+
+
+});
+
+const bestSwiper = new Swiper('#bestTitle .swiper', {
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
   },
-  spaceBetween: 300,
-  // 반복
-  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5.5,
+  loop: false,
   
   direction: 'horizontal',
   pagination: {
-    el: '#promotionSwiper .swiper-pagination',
+    el: '#bestTitle .swiper-pagination',
   },
   navigation: {
-    nextEl: '#promotionSwiper .swiper-button-next',
-    prevEl: '#promotionSwiper .swiper-button-prev',
+    nextEl: '#bestTitle .swiper-button-next',
+    prevEl: '#bestTitle .swiper-button-prev',
   }
 });
+
+const review = new Swiper('#photoRvbox .swiper', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 4,
+  centeredSlides: true,
+  spaceBetween: 6,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '#photoRvbox .swiper-pagination',
+    clickable: true,
+  },
+
+
+});
+
+/// ScrollMagic : 스크롤 됬을때 애니메이션 실행하기
+const spyEls = document.querySelectorAll('.scroll-spy')
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 뷰포트 상단에서 80% 스크롤됬을때 실행
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+// AOS
+AOS.init({
+  duration: 600,
+})
+
