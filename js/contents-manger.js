a2321ac7726edb7414fd597c65c5bb8e42f261c6
YoHaiYo <12si47bun@naver.com>
2023-11-20 16:25:10 +0900

    베스트 아이템 데이터로 관리화 작업

---
+++ js/contents-manger.js
@@ -0,0 +1,269 @@
+/// itemTitle 컨텐츠 관리
+const itemTitleContents = [
+  {
+    categorydata: "gwashilju",
+    img: "./img/과실주/샤인머스켓와인.jpg",
+    productName: "샤인머스켓와인",
+    productPrice: "12000원",
+    starNum: 4,
+    desc: "달콤한 샤인머스켓 향이 듬뿍~"
+  },   
+  {
+    categorydata: "jeungryuju",
+    img: "./img/증류주/겨울소주.jpg",
+    productName: "겨울소주",
+    productPrice: "12000원",
+    starNum: 3,
+    desc: "겨울을 머금은 그맛..!"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/유자주.jpg",
+    productName: "유자주",
+    productPrice: "12000원",
+    starNum: 4,
+    desc: "유자향이 듬뿍!"
+  },   
+  {
+    categorydata: "gwashilju",
+    img: "./img/과실주/심쿵하리 사과.jpg",
+    productName: "심쿵하리 사과",
+    productPrice: "12000원",
+    starNum: 4,
+    desc: "사과 99.9999%"
+  },   
+  {
+    categorydata: "gwashilju",
+    img: "./img/과실주/복숭아 와인.jpg",
+    productName: "복숭아 와인",
+    productPrice: "12000원",
+    starNum: 4,
+    desc: "말랑한 복숭아를 마시는 기분!"
+  },   
+  {
+    categorydata: "jeungryuju",
+    img: "./img/증류주/꿀샘.jpg",
+    productName: "꿀샘",
+    productPrice: "12000원",
+    starNum: 4,
+    desc: "꿀꿀꿀꿀꿀맛"
+  },   
+  {
+    categorydata: "takju",
+    img: "./img/탁주/대대포 블루.jpg",
+    productName: "대대포 블루",
+    productPrice: "3610원",
+    starNum: 5,
+    desc: "맑고 달콤한 막걸리"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/yG3P-1698802864627-jonghap_thumb.jpg",
+    productName: "종합 담화박스",
+    productPrice: "52000원",
+    starNum: 4,
+    desc: "술담화 종합 선물세트"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/q4rf-1698803663930-jeungryu_thumb.jpg",
+    productName: "증류주 담화세트",
+    productPrice: "55000원",
+    starNum: 4,
+    desc: "고도수를 즐기는 술고래들을 위한"
+  },   
+  {
+    categorydata: "Imgbox set",
+    img: "./img/set/o1Cx-1698804322829-yakju_thumb.jpg",
+    productName: "약청주 담화세트",
+    productPrice: "55000원",
+    starNum: 3,
+    desc: "약주 청주로 구성된 담화세트"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/1bZF-1680445939882-sweet+home+thumb.jpg",
+    productName: "피로회복세트",
+    productPrice: "36900원",
+    starNum: 4,
+    desc: "부모님 선물로 딱이에요!"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/64X8-1647428355883-보리수, 매화 선물세트-2.jpg",
+    productName: "이밤담다 선물세트",
+    productPrice: "26000원",
+    starNum: 5,
+    desc: "맛과 감성 모두 놓칠 수없다"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/deTI-1666837157875-sum.jpg",
+    productName: "술자리를 여는 소주들",
+    productPrice: "20600원",
+    starNum: 3,
+    desc: "쌀로 만든 진짜 소주!"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/OH2l-1642147642650-apsd0.jpg",
+    productName: "애플사이더 4종 스타터팩",
+    productPrice: "22900원",
+    starNum: 4,
+    desc: "사과로 만든 스파클링 과실주"
+  },   
+  {
+    categorydata: "set",
+    img: "./img/set/9IDS-1666256226957-thumb.jpg",
+    productName: "샤토미소 미니어처 와인 6종 세트",
+    productPrice: "42000원",
+    starNum: 5,
+    desc: "6가지 맛을 한번에!"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/국화주.jpg",
+    productName: "국화주",
+    productPrice: "20000원",
+    starNum: 5,
+    desc: "가을에 어울리는 술"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/니노메.jpg",
+    productName: "니모메",
+    productPrice: "7700원",
+    starNum: 4,
+    desc: "귤피가 들어간 술"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/능이주.jpg",
+    productName: "능이주",
+    productPrice: "9900원",
+    starNum: 3,
+    desc: "약이 되는 술"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/솔송주.jpg",
+    productName: "솔송주",
+    productPrice: "8550원",
+    starNum: 4,
+    desc: "상쾌한 솔향이 입안 가득!"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/서설.jpg",
+    productName: "서설",
+    productPrice: "13500원",
+    starNum: 5,
+    desc: "눈처럼 맑은 맛의 술"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/청명주.jpg",
+    productName: "청명주",
+    productPrice: "19500원",
+    starNum: 4,
+    desc: "청와대의 선택"
+  },   
+  {
+    categorydata: "cheongju",
+    img: "./img/청주/동학.jpg",
+    productName: "동학",
+    productPrice: "3420원",
+    starNum: 5,
+    desc: "구수한 쌀의 맛"
+  },   
+  {
+    categorydata: "gwashilju",
+    img: "./img/과실주/산머루주.jpg",
+    productName: "산머루주",
+    productPrice: "5700원",
+    starNum: 5,
+    desc: "진한 달콤함"
+  },   
+  {
+    categorydata: "gwashilju",
+    img: "./img/과실주/오디주.jpg",
+    productName: "오디주",
+    productPrice: "10200원",
+    starNum: 5,
+    desc: "맛있는 술은 오디있지?"
+  },   
+  {
+    categorydata: "",
+    img: "",
+    productName: "",
+    productPrice: "",
+    starNum: 4,
+    desc: ""
+  },   
+  {
+    categorydata: "",
+    img: "",
+    productName: "",
+    productPrice: "",
+    starNum: 4,
+    desc: ""
+  },   
+  {
+    categorydata: "",
+    img: "",
+    productName: "",
+    productPrice: "",
+    starNum: 4,
+    desc: ""
+  },   
+  {
+    categorydata: "",
+    img: "",
+    productName: "",
+    productPrice: "",
+    starNum: 4,
+    desc: ""
+  },   
+  {
+    categorydata: "",
+    img: "",
+    productName: "",
+    productPrice: "",
+    starNum: 4,
+    desc: ""
+  },   
+  
+]
+
+$(document).ready(function(){
+  
+
+  let makeContents = '';
+  for (i in itemTitleContents) {
+    let stars = '';
+  function printStars(starNum) {
+    for (let i = 0; i < starNum; i++) {
+      stars += '<span></span>';
+    }
+    return stars;
+  }
+    // ★data속성에 .을 붙여서 클래스로 인식되게하기.
+    makeContents +=`
+    <div class="Imgbox ${itemTitleContents[i].categorydata}">
+              <a href="">
+                <span><img src="${itemTitleContents[i].img}" alt=""></span>
+                <div class="ItemDesc">
+                  <div class="productName ">${itemTitleContents[i].productName}</div>
+                  <div class="productPrice ">${itemTitleContents[i].productPrice}</div>
+                  <div class="starNum d-flex">
+                    ${printStars(itemTitleContents[i].starNum)}
+                  </div>
+                  <p class="desc">${itemTitleContents[i].desc}</p>
+                </div>
+              </a>
+            </div>`}
+
+ document.querySelector('#itemTitle .itemImgWrap_margin').innerHTML = makeContents;
+
+})
+
