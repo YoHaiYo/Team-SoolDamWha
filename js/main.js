d53110923127f381ef723c931166a62a49d52f0a
User -2023WXXOU\Administrator <1000tmddms@gmail.com>
2023-11-21 18:06:04 +0900

    상단 태그구조 동적개체로 데이터화

--- js/main.js
+++ js/main.js
@@ -51,67 +51,243 @@ $(document).ready(function () {
   })
 
 })
-
+// 글로벌-----------------------------------------------
 const langResource = {
 
   ko: {
-    subs: "구독",
-    total: "전체상품",
-    best: "베스트",
-    event: "이벤트",
-    signin: "로그인",
-    signup: "회원가입",
-    serv: "구독, 아직도 망설이시나요?",
-    firstb: "첫 구매시 샘플 술 증정",
-    saleE: "첫 구독은 10% 할인!",
-    pointup: "리뷰 쓰면 쌓이는 포인트!",
+    gnb: [
+      {
+        nm: "구독",
+        href: "#",
+        cls: "",
+        target: ""
+      },
+      {
+        nm: "전체상품",
+        href: "#itemTitle",
+        cls: "scrolla",
+        target: ""
+      },
+      {
+        nm: "베스트",
+        href: "#bestTitle",
+        cls: "scrolla",
+        target: ""
+      },
+      {
+        nm: "이벤트",
+        href: "#",
+        cls: "",
+        target: ""
+      },
+      {
+        nm: "Q & A",
+        href: "",
+        cls: "",
+        target: ""
+      }
+
+    ],
+    login: [
+      {
+        nm: "로그인",
+        href: "",
+        cls: "mx-3",
+        target: ""
+      },
+      {
+        nm: "회원가입",
+        href: "",
+        cls: "mx-3",
+        target: ""
+      }
+
+    ],
+    promotion: [
+      {
+        scls: "bgimg1",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "첫 구매시 샘플 술 증정",
+          p: "신규 가입하고 | 궁금했던 술 체험해보세요."
+        }
+      },
+      {
+        scls: "bgimg2",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "첫 구독은 10% 할인!",
+          p: "신규 가입 혜택받고 | 소믈리에가 엄선한 전통주를 | 매월 경험하세요."
+        }
+      },
+      {
+        scls: "bgimg3",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "리뷰 쓰면 쌓이는 포인트!",
+          p: "sns 후기 인증 시 2,000원 적립"
+        }
+      }
+    ]
+
+    //     subs: "구독",
+    //     total: "전체상품",
+    //     best: "베스트",
+    //     event: "이벤트",
+    //     signin: "로그인",
+    //     signup: "회원가입",
+    //     serv: "구독, 아직도 망설이시나요?",
+    //     firstb: "첫 구매시 샘플 술 증정",
+    //     saleE: "첫 구독은 10% 할인!",
+    //     pointup: "리뷰 쓰면 쌓이는 포인트!",
+
+
+
 
 
 
 
   },
   en: {
-    subs: "Subscribe",
-    total: "Items",
-    best: "Best",
-    event: "Event",
-    signin: "Sign In",
-    signup: "Sign Up",
-    serv: "Still Hesitant?",
-    firstb: "Beginner For Sample Liquid",
-    saleE: "10% Sale For First Subs!",
-    pointup: "Review For Your Accumulate"
+    gnb: [
+      {
+        nm: "Subscribe",
+        href: "#",
+        cls: "",
+        target: ""
+      },
+      {
+        nm: "Items",
+        href: "#itemTitle",
+        cls: "scrolla",
+        target: ""
+      },
+      {
+        nm: "Best",
+        href: "#bestTitle",
+        cls: "scrolla",
+        target: ""
+      },
+      {
+        nm: "Event",
+        href: "#",
+        cls: "",
+        target: ""
+      }
+
+    ],
+    login: [
+      {
+        nm: "Sign In",
+        href: "",
+        cls: "mx-3",
+        target: ""
+      },
+      {
+        nm: "Sign Up",
+        href: "",
+        cls: "mx-3",
+        target: ""
+      }
+
+    ],
+    promotion: [
+      {
+        scls: "bgimg1",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "Beginner For Sample Liquid",
+          p: "신규 가입하고 | 궁금했던 술 체험해보세요."
+        }
+      },
+      {
+        scls: "bgimg2",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "10% Sale For First Subs!",
+          p: "신규 가입 혜택받고 | 소믈리에가 엄선한 전통주를 | 매월 경험하세요."
+        }
+      },
+      {
+        scls: "bgimg3",
+        ccls: "effecttext",
+        href: "",
+        content: {
+          strong: "Review For Your Accumulate",
+          p: "sns 후기 인증 시 2,000원 적립"
+        }
+      }]
 
   }
+
+
+
+
+  //   en: {
+  //     subs: "Subscribe",
+  //     total: "Items",
+  //     best: "Best",
+  //     event: "Event",
+  //     signin: "Sign In",
+  //     signup: "Sign Up",
+  //     serv: "Still Hesitant?",
+  //     firstb: "Beginner For Sample Liquid",
+  //     saleE: "10% Sale For First Subs!",
+  //     pointup: "Review For Your Accumulate",
+
+
+  //   }
 }
 
-let mychoic = document.getElementById("languageSelector").value;
-updateLanguage(mychoic);
 
-document.getElementById("languageSelector").addEventListener("change", function () {
-  mychoic = this.value;
-  updateLanguage(mychoic);
-});
 
-function updateLanguage(lang) {
-  for (let key in langResource[lang]) {
+let gnbtags = '';
 
-    const element = document.getElementById(key);
-
-    if (element) {
-      element.textContent = langResource[lang][key];
-
-    }
-  }
+for (x in langResource.ko.gnb) {
+  gnbtags += `<li><a href="${langResource.ko.gnb[x].href}" class="${langResource.ko.gnb[x].cls}">${langResource.ko.gnb[x].nm}</a></li>`
 }
 
 
 
-// $("#topbtn").click(function (x) {
-//   x.preventDefault();
-//   targetnm = "body";
-//   $("body, html").stop(true, true).animate({ scrollTop: $(targernm).offset().top })
-// })
+$("#gnb").html(gnbtags)
+
+let logintags = "";
+
+for (x in langResource.ko.login) {
+  logintags += `<li class="${langResource.ko.login[x].cls}"><a href=${langResource.ko.login[x].href}#">${langResource.ko.login[x].nm}</a></li>`
+}
+
+$("#login").prepend(logintags)
+
+
+// let mychoic = document.getElementById("languageSelector").value;
+// updateLanguage(mychoic);
+
+// document.getElementById("languageSelector").addEventListener("change", function () {
+//   mychoic = this.value;
+//   updateLanguage(mychoic);
+// });
+
+// function updateLanguage(lang) {
+//   for (let key in langResource[lang]) {
+
+//     const element = document.getElementById(key);
+
+//     if (element) {
+//       element.textContent = langResource[lang][key];
+
+//     }
+//   }
+// }
+
+// 글로벌-----------------------------------------------
+
+
 
 
 
